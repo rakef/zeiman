07e6655bf0fe5d178767817c96803506ec58c580
rakef <rakefet.zeiman@gmail.com>
2018-02-26 01:42:11 +0200

    random story on stories, zoom on hovering on imgs in articles

---
+++ script.js
@@ -0,0 +1,17 @@
+function randomStory() {
+    // alert(Math.ceil(Math.random))
+
+
+    var storyNumber = (Math.ceil(Math.random() * 36))
+    window.location.href = "story" + storyNumber + ".html"
+
+
+    //    var url = "http://www.mypage.com/index.php?rnd="+Math.random()
+
+
+
+
+    // console.log(storyNumber);
+    // window.location.href = "zeiman.com/stories/story+(storyNumber).html";
+
+}
